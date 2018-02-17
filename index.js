edbe4544bb2857f5dcc10339b706df48506ed5c5
jennitz <jennitz@gmail.com>
2018-02-17 16:47:11 -0500

    functional reserved tables

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 $(document).ready(() => {
   $('form').hide();
+  var currentTable;
 
+
+  $('.row').on('click', '.available', function(){
+    var tableNum = $(this).text();
+    currentTable = $(this);
+    $('#table').html(`Table Number ${tableNum}`);
+    $('form').fadeToggle('slow');
+  });
+
+
+  $('#cancelbtn').on('click', function(){
+    $('form').hide();
+  });
+
+  $('#submit').on('click', function(){
+    // var name = $('#name').val();
+    // var phone = $('#phone').val();
+    // var seats = $('#seats').val();
+
+    currentTable.attr('class', 'reserved');
+    $('form').hide();
+    // currentTable.unbind('hover');
+    // $
+  });
+  // if (table is reserved){
+  //   click & hover disabled and .css('background-color', '#696969')
+  // }
 });
