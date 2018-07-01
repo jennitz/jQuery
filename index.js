d4614329c4073cb6bbc56158143372b123a1250d
jennitz <jennitz@gmail.com>
2018-07-01 01:19:22 -0400

    completed project

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
 $(document).ready(() => {
-  $('.form').hide();
   var currentTable;
-  $('p').hide();
 
-
-  $('.row').on('click', '.available', function(){
-    var tableNum = $(this).text();
+  $('.available').on('click', function(){
+    $('#table-number').append(this.id);
+    $('#contact').hide().delay(200).fadeIn(400);
+    $('#reservationdiv').css('display', 'flex');
     currentTable = $(this);
-    $('#table').html(`Table Number ${tableNum}`);
-    $('.form').fadeToggle('slow');
-  });
-
-
-  $('#cancelbtn').on('click', function(){
-    $('.form').hide();
+    // $('.form').fadeToggle('slow');
   });
 
   $('#reservebtn').on('click', function(){
-    var name = $('#name').val();
-    // var phone = $('#phone').val();
-    var seats = $('#seats').val();
-    currentTable.attr('class', 'reserved');
-    $('.form').hide();
+    var input1 = $('#name');
+    var input2 = $('#phone');
+    var input3 = $('#seats');
+    // currentTable.attr('class', 'reserved');
     // currentTable.attr('p', `Name: ${name}`<br />`Party Size: ${seats}`);
-    $('p').show();
+    // $('.form').hide();
+    // $('p').show();
+
+    $('#contact').fadeOut(500);
+    var hoverDiv = `<div class='tool-tip'>
+    <p>Name: ${input1.val()}</p>
+    <p>Seats: ${input3.val()}</p>
+    </div>`
+    currentTable.append(hoverDiv);
+    currentTable.addClass('reserved');
+    currentTable.removeClass('available');
+    input1.val('');
+    input2.val('');
+    input3.val('');
+    $('#table-number').empty();
+
+  if($('.reserved')){
+    $('#clearbtn').removeClass('hidden');
+    $('#clearbtn').on('click', function(){
+      input1.val('');
+      input2.val('');
+      input3.val('');
+      $(this).parent().parent().fadeOut(500);
+      $('#clearbtn').addClass('hidden');
+      currentTable.removeClass('reserved');
+      currentTable.addClass('available');
+      $('table-number').empty();
+      currentTable.remove(hoverDiv);
+    })
+  }
   });
 
-  // $('.reserved').on('hover', function(){
-  //   // $(this).unbind('cursor');
-  //   //how to specify currentTable's <p>?
-  //   $('p').text(`Name: ${name}`<br />`Party Size: ${seats}`);
-  //   $('p').fadeToggle('slow');
-  // });
+  $('#cancelbtn').on('click', function(){
+    $(this).parent().parent().fadeOut(500);
+    $('table-number').empty();
+  });
 
 });
